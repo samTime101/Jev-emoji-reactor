e6a4ac06819f46027ec16cd773dccdb63c5c7842
samTime101 <srijanregmi44@gmail.com>
2026-09-22 22:31:08 +0545

    feat: init from emoji/

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
-import os
-import json
-from typesafe_sdk import TypeSafeClient, Choice
-from dotenv import load_dotenv
-from pydantic import BaseModel
-
-load_dotenv()
-
-class EmojiResult(BaseModel):
-    input: str
-    emoji: str
-    confidence: float
-
-class EmojiClassifier:
-    def __init__(self,text):
-        self.text = text
-        self.client = TypeSafeClient(api_key=os.environ.get("TYPESAFE_API_KEY"))
-
-    def predict(self):
-        with open("emojis.json", "r", encoding="utf-8") as f:
-            emojis = json.load(f)
-        self.emoji_criteria = {emoji: description for emoji, description in emojis.items()}
-        question = Choice(instructions="Choose the emoji that best matches the sentiment of the text.",criteria=self.emoji_criteria)
-        response = self.client.system_one({"text": self.text},{"emoji": question},)
-        choice = response.choices["emoji"]
-
-        return EmojiResult(input=self.text, emoji=choice.choice, confidence=choice.confidence)
+from emoji.main import EmojiClassifier
 
 emojiclassifier = EmojiClassifier("Hello I am samip")
 result = emojiclassifier.predict()
